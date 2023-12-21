362e4be32c42395afe8863df04a1f83c5e9ae611
Carson <carson.sturtevant@gmail.com>
2023-12-21 09:18:56 -0700

    Blog page testing routing

---
+++ app/blog/page.tsx
@@ -0,0 +1,10 @@
+export default function Blog() {
+    return (
+        <>
+            <main>
+                <h1 className={"font-bold p-6"}>Blog</h1>
+                <p className={"p-6"}>Welcome to the blog!</p>
+            </main>
+        </>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: "Home Page",
+    description: "Super cool new social media platform!"
+};
+
 export default function Home() {
-  return (
-    <main></main>
-  );
+    return (
+        <main></main>
+    );
 }
